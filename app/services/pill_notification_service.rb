3aa4e3f04dac565a7c80fe6af85a15aa6f512055
Alexey Naumov <alexsnaumov@gmail.com>
2020-05-03 18:06:51 +0300

    Keep on travis

--- app/services/pill_notification_service.rb
+++ app/services/pill_notification_service.rb
@@ -23,7 +23,8 @@ class PillNotificationService
   attr_reader :current_hour
 
   def notify_current_hour
-    puts 'notify_current_hour', users_scope(0).count
+    puts User.last.as_json
+    puts current_hour
     send_messages(users: users_scope(0), message_slug: :daily_first)
   end
 
