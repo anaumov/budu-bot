66c3f842d516d236beb38861e3e4beada7efb733
Alexey Naumov <alexsnaumov@gmail.com>
2020-05-07 17:40:59 +0300

    Fix bugsnag config

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,5 +2,5 @@
 
 Bugsnag.configure do |config|
   config.api_key = Secrets[:bugsnag_api_key]
-  # config.notify_release_stages = %w[staging production]
+  config.notify_release_stages = %w[staging production]
 end
