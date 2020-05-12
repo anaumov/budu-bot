a997113e0cfed9cf56910cd43a8187b2afa6df50
Alexey Naumov <alexsnaumov@gmail.com>
2020-05-12 08:08:01 +0300

    Fix viral load graph polygon

--- app/services/messages_service.rb
+++ app/services/messages_service.rb
@@ -7,7 +7,7 @@ class MessagesService
       immune_status = results.find(&:immune_status?)
       viral_load = results.find(&:viral_load?)&.value&.to_s || ''
       viral_load = viral_load.size < 6 ? (viral_load + ' ' * 2 * (6 - viral_load.size)) : viral_load
-      result += "#{date.strftime('%d.%m.%y')}  #{viral_load}  #{immune_status&.value}\n"
+      result += "#{date.strftime('%d.%m.%Y')}  #{viral_load}  #{immune_status&.value}\n"
     end
     result
   end
