c8a71381ffeb917ae43c49364e87013bb9f46f2c
Alexey Naumov <alexsnaumov@gmail.com>
2020-05-14 14:00:25 +0300

    Remove preparing message

--- app/controllers/concerns/telegram_graph_concern.rb
+++ app/controllers/concerns/telegram_graph_concern.rb
@@ -4,9 +4,10 @@ module TelegramGraphConcern
   private
 
   def respond_with_graph
-    respond_with :message, text: 'Готовлю график...'
+    prepearing_message = respond_with(:message, text: 'Подготовка графика...').dig('result')
     service = GraphService.new(current_user)
     graph_file = service.render_image
+    bot.delete_message(message_id: prepearing_message['message_id'], chat_id: prepearing_message['chat']['id'])
     respond_with :photo, photo: graph_file
     service.remove_file(graph_file)
   end
