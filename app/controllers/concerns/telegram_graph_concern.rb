d8fe646108e2e8485b7387f4f339844c597781ea
Alexey Naumov <alexsnaumov@gmail.com>
2020-05-21 14:17:53 +0300

    Update graph quality

--- app/controllers/concerns/telegram_graph_concern.rb
+++ app/controllers/concerns/telegram_graph_concern.rb
@@ -11,9 +11,9 @@ module TelegramGraphConcern
 
     send_prepearing_message
     graph_file = graph_service.render_image
+    respond_with :photo, photo: graph_file
     remove_preparing_message
-    # respond_with :photo, photo: graph_file
-    respond_with :document, document: graph_file
+    # respond_with :document, document: graph_file
     graph_service.remove_file(graph_file)
   end
 
--- app/models/graph.rb
+++ app/models/graph.rb
@@ -2,11 +2,16 @@
 
 class Graph
   def self.canvas_width
-    630
+    630 * 2
   end
 
   def self.canvas_height
-    350
+    350 * 2
+  end
+
+  # NOTE: use as html font-size
+  def self.base
+    canvas_width / 63
   end
 
   def self.header_height
@@ -14,11 +19,11 @@ class Graph
   end
 
   def self.graph_height
-    254
+    (canvas_height * 0.73).ceil
   end
 
   def self.chart_height
-    (graph_height * 0.92).ceil
+    (graph_height * 0.95).ceil
   end
 
   def self.chart_width
